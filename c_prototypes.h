1f915b4bdcb72069b8070c6269d3b8791cde1ae9
Jeff Hammond <jeff.science@gmail.com>
2024-01-29 17:48:04 +0200

    sizes

--- c_prototypes.h
+++ c_prototypes.h
@@ -1,7 +1,15 @@
 #ifndef C_PROTOTYPES_H
 #define C_PROTOTYPES_H
 
+#include <math.h>
+#include <complex.h>
+
 // koota_<op>_<type>
 // koota_<op>_<type>_c
+
+void koota_sum_complex4(_Complex float * invec, _Complex float * inoutvec, size_t len);
+void koota_max_real4(float * invec,float * inoutvec, size_t len);
+void koota_band_integer4(int32_t * invec, int32_t * inoutvec, size_t len);
+void koota_land_logical4(int32_t * invec, int32_t * inoutvec, size_t len);
 
 #endif // C_PROTOTYPES_H
--- koota.F90
+++ koota.F90
@@ -1,71 +1,71 @@
-subroutine koota_sum_complex(invec, inoutvec, len) &
-    bind(C,name="koota_sum_complex")
-    use, intrinsic :: iso_c_binding, only : c_ptr, c_f_pointer
+subroutine koota_sum_complex4(invec, inoutvec, len) &
+    bind(C,name="koota_sum_complex4")
+    use, intrinsic :: iso_c_binding, only : c_ptr, c_size_t, c_f_pointer
     implicit none
     type(c_ptr), intent(in) :: invec
     type(c_ptr), intent(inout) :: inoutvec
-    integer, intent(in), value :: len
+    integer(kind=c_size_t), intent(in), value :: len
 
-    integer :: i
-    complex, pointer, dimension(:), contiguous :: fp_invec, fp_inoutvec
+    integer(kind=c_size_t) :: i
+    complex(4), pointer, dimension(:), contiguous :: fp_invec, fp_inoutvec
     call C_F_POINTER(invec,fp_invec,[len])
     call C_F_POINTER(inoutvec,fp_inoutvec,[len])
 
     do i=1,len
         fp_inoutvec(i) = fp_inoutvec(i) + fp_invec(i)
     end do
-end subroutine koota_sum_complex
+end subroutine koota_sum_complex4
 
-subroutine koota_max_real(invec, inoutvec, len) &
-    bind(C,name="koota_max_real")
-    use, intrinsic :: iso_c_binding, only : c_ptr, c_f_pointer
+subroutine koota_max_real4(invec, inoutvec, len) &
+    bind(C,name="koota_max_real4")
+    use, intrinsic :: iso_c_binding, only : c_ptr, c_size_t, c_f_pointer
     implicit none
     type(c_ptr), intent(in) :: invec
     type(c_ptr), intent(inout) :: inoutvec
-    integer, intent(in), value :: len
+    integer(kind=c_size_t), intent(in), value :: len
 
-    integer :: i
-    real, pointer, dimension(:), contiguous :: fp_invec, fp_inoutvec
+    integer(kind=c_size_t) :: i
+    real(4), pointer, dimension(:), contiguous :: fp_invec, fp_inoutvec
     call C_F_POINTER(invec,fp_invec,[len])
     call C_F_POINTER(inoutvec,fp_inoutvec,[len])
 
     do i=1,len
         fp_inoutvec(i) = MAX(fp_inoutvec(i),fp_invec(i))
     end do
-end subroutine koota_max_real
+end subroutine koota_max_real4
 
-subroutine koota_band_integer(invec, inoutvec, len) &
-    bind(C,name="koota_band_integer")
-    use, intrinsic :: iso_c_binding, only : c_ptr, c_f_pointer
+subroutine koota_band_integer4(invec, inoutvec, len) &
+    bind(C,name="koota_band_integer4")
+    use, intrinsic :: iso_c_binding, only : c_ptr, c_size_t, c_f_pointer
     implicit none
     type(c_ptr), intent(in) :: invec
     type(c_ptr), intent(inout) :: inoutvec
-    integer, intent(in), value :: len
+    integer(kind=c_size_t), intent(in), value :: len
 
-    integer :: i
-    integer, pointer, dimension(:), contiguous :: fp_invec, fp_inoutvec
+    integer(kind=c_size_t) :: i
+    integer(4), pointer, dimension(:), contiguous :: fp_invec, fp_inoutvec
     call C_F_POINTER(invec,fp_invec,[len])
     call C_F_POINTER(inoutvec,fp_inoutvec,[len])
 
     do i=1,len
         fp_inoutvec(i) = IAND(fp_inoutvec(i),fp_invec(i))
     end do
-end subroutine koota_band_integer
+end subroutine koota_band_integer4
 
-subroutine koota_land_logical(invec, inoutvec, len) &
-    bind(C,name="koota_and_logical")
-    use, intrinsic :: iso_c_binding, only : c_ptr, c_f_pointer
+subroutine koota_land_logical4(invec, inoutvec, len) &
+    bind(C,name="koota_and_logical4")
+    use, intrinsic :: iso_c_binding, only : c_ptr, c_size_t, c_f_pointer
     implicit none
     type(c_ptr), intent(in) :: invec
     type(c_ptr), intent(inout) :: inoutvec
-    integer, intent(in), value :: len
+    integer(kind=c_size_t), intent(in), value :: len
 
-    integer :: i
-    logical, pointer, dimension(:), contiguous :: fp_invec, fp_inoutvec
+    integer(kind=c_size_t) :: i
+    logical(4), pointer, dimension(:), contiguous :: fp_invec, fp_inoutvec
     call C_F_POINTER(invec,fp_invec,[len])
     call C_F_POINTER(inoutvec,fp_inoutvec,[len])
 
     do i=1,len
         fp_inoutvec(i) = fp_inoutvec(i) .AND. fp_invec(i)
     end do
-end subroutine koota_land_logical
+end subroutine koota_land_logical4
