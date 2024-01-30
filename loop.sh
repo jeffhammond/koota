49522e405067d4695b4ad1f7ae1b431d867a36bc
Jeff Hammond <jeff.science@gmail.com>
2024-01-30 10:11:51 +0200

    support real10 etc

--- loop.sh
+++ loop.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-for t in real complex integer logical ; do
+for t in integer logical ; do
     for s in 1 2 4 8 16 ; do
+        for op in min max sum prod band bor bxor land lor lxor ; do
+            ./generate.sh $t $s $op > ${t}${s}_${op}.F90
+            make ${t}${s}_${op}.o >& /dev/null || rm ${t}${s}_${op}.F90
+        done
+    done
+done
+
+for t in real complex ; do
+    for s in 1 2 4 8 10 16 ; do
         for op in min max sum prod band bor bxor land lor lxor ; do
             ./generate.sh $t $s $op > ${t}${s}_${op}.F90
             make ${t}${s}_${op}.o >& /dev/null || rm ${t}${s}_${op}.F90
@@ -12,7 +21,7 @@ done
 rm -f koota.F90
 
 for t in real complex integer logical ; do
-    for s in 1 2 4 8 16 ; do
+    for s in 1 2 4 8 10 16 ; do
         for op in min max sum prod band bor bxor land lor lxor ; do
             if [ -f ${t}${s}_${op}.F90 ] ; then
                 cat ${t}${s}_${op}.F90 >> koota.F90
